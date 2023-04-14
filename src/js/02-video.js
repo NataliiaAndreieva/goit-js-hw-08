634ce484ab88e3c027877c102b2b535423a5df57
NataliiaAndreieva <taliandreeva@gmail.com>
2023-04-14 18:53:22 +0300

    home-work-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+  const iframe = document.querySelector('iframe');
+  const player = new Player(iframe);
+
+
+function onPlay(data) {
+  localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+}
+
+ player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+const { email, message } = form.elements;
+
+populateForm();
+
+function onFormData(evt) {
+  formData = { email: email.value, message: message.value };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  if (formData) {
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.dir({ email: email.value, message: message.value });
+
+   if (email.value === '' || message.value === '') {
+     return alert('Всі поля мають бути заповнені');
+   }
+
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
+  formData = {};
+ }
